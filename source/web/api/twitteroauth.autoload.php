b40731e915b9aa1e1e2bf09d60e85f4a508d0ddd
道化師 <wraith@trickpalace.net>
2016-03-07 02:09:09 +0900

    abraham\twitteroauth 用 autoloader

---
+++ source/web/api/twitteroauth.autoload.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('abraham/twitteroauth/Token.php');
+require_once('abraham/twitteroauth/TwitterOAuthException.php');
+require_once('abraham/twitteroauth/Util.php');
+require_once('abraham/twitteroauth/Request.php');
+require_once('abraham/twitteroauth/Consumer.php');
+require_once('abraham/twitteroauth/SignatureMethod.php');
+require_once('abraham/twitteroauth/HmacSha1.php');
+require_once('abraham/twitteroauth/Response.php');
+require_once('abraham/twitteroauth/Config.php');
+require_once('abraham/twitteroauth/Util/JsonDecoder.php');
+require_once('abraham/twitteroauth/TwitterOAuth.php');
+
+/**
+ * Use to autoload needed classes without Composer.
+ *
+ * @param string $class The fully-qualified class name.
+ * @return void
+ */
+spl_autoload_register(function ($class) {
+
+    // project-specific namespace prefix
+    $prefix = 'Abraham\\TwitterOAuth\\';
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/abraham\/twitteroauth/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+    $relative_class = substr($class, $len);
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
