473fdc79190a348390d1076a90a5a345d43aa1d6
道化師 <wraith@trickpalace.net>
2016-03-16 09:47:29 +0900

    "0" が "" になってしまう問題の修正

--- source/web/api/common/string.php
+++ source/web/api/common/string.php
@@ -2,5 +2,10 @@
 
 function typesafe_iconv_substr($str, $offset, $length)
 {
-	return iconv_substr($str, $offset, $length) ?: "";
+	$result = iconv_substr($str, $offset, $length);
+	if (false === $result)
+	{
+		$result = "";
+	}
+	return $result;
 }
